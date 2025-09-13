97830fdbee2dc0988f9a0ec1ca29fcf4c26a23bd
Styris <styris@styris.net>
2025-09-12 22:44:06 -0700

    Update to v0.8.1

--- src/compile.bat
+++ src/compile.bat
@@ -6,6 +6,8 @@ for /f "usebackq delims=" %%A in (`type "playbook.conf" ^| findstr /c:"<Version>
 
 del /q /f "Privacy+ v%versionXml:~10,-10%.apbx"
 7z a "Privacy+ v%versionXml:~10,-10%.apbx" playbook.conf Executables Configuration Images -pmalte
+rem 7z a -spf -y -mx1 -pmalte -tzip "Privacy+ v%versionXml:~10,-10%.apbx" playbook.conf Executables Configuration Images
+
 if %ERRORLEVEL% NEQ 0 (
 	pause
 )
